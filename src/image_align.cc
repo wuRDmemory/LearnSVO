cfbcb3b7858e0f7b1ebe188c4841c7577714f8a8
wuRDmemory <wuch2039@163.com>
2019-09-10 08:00:49 +0800

    update image align

--- src/image_align.cc
+++ src/image_align.cc
@@ -1,14 +1,54 @@
 #include "image_align.hpp"
 
+namespace {
+    // class ImageAlignCostFunction : public ceres::SizedCostFunction<1, 7> {
+    // public:
+    //     ImageAlignCostFunction(const double refInstense, const double curInstense, 
+    //                             Eigen::Matrix<double, 6, 1>& J) {
+    //         mRefBlock = refBlock;
+    //         mCurBlock = curBlock;
+    //         mXYZ = xyz;
+    //         mRefFrame = refFrame;
+    //     }
+    //     virtual ~ImageAlignCostFunction() {}
+    //     virtual bool Evaluate(double const* const* parameters,
+    //                             double* residuals,
+    //                             double** jacobians) const {
+    //         residuals[0] =  mCurInstense - mRefInstense;
+
+    //         // Compute the Jacobian if asked for.
+    //         if (jacobians != NULL){
+    //             if (jacobians[0] != NULL) {
+    //                 jacobians[0][0] = -1;
+    //             }
+    //         }
+    //         return true;
+    //     }
+    // private:
+    //     double mRefInstense, mCurInstense;
+    //     Eigen::MatrixXf mJ;
+    // };
+}
+
 namespace mSVO {
-    #define INSIDEIMAGE(x, dx, min, max) return (x-dx) >= (min) and (x+dx) < (max)
+    #define INSIDEIMAGE(x, dx, min, max) ((x-dx) >= (min) and (x+dx) < (max))
 
     ImageAlign::ImageAlign(int minlevel, int maxlevel, int iterCnt): mMinLevel(minlevel), mMaxLevel(maxlevel), mIterCnt(iterCnt){
-        ;
+        meps = 1e-7;
+        mMaxFailThr = 5;
     }
 
     ImageAlign::~ImageAlign() {
         ;
+    }
+
+    void ImageAlign::run(FramePtr refFrame, FramePtr curFrame) {
+        mTc_r_new = curFrame->pose().inverse()*refFrame->pose();
+        mTc_r_old = mTc_r_new;
+        for (int level = mMinLevel; level <mMaxLevel; level++) {
+            optimize(refFrame, curFrame, level);
+        }
+        curFrame->pose() = mTc_r_new;
     }
 
     void ImageAlign::prepareData(FramePtr refFrame, int level) {
@@ -22,8 +62,8 @@ namespace mSVO {
 
         auto& features = refFrame->obs();
         mVisables.resize(features.size(), true);
-        mRefPatchCache = MatrixXf(features.size(),   patchArea);
-        mJacobianCache = MatrixXf(features.size()*patchArea, 6);
+        mRefPatchCache.resize(features.size(),   patchArea);
+        mJacobianCache.resize(features.size()*patchArea, 6);
         int feature_cnt = 0;
         for (auto begin = features.begin(); begin != features.end(); ++begin, ++feature_cnt) {
             Vector2f fPx = (*begin)->mPx*scale;
@@ -56,7 +96,7 @@ namespace mSVO {
             for (int i = 0; i < patchSize; i++) {
                 uchar* img_ptr = (uchar*)ref_img.data + (v_iref+i-halfPatchSize)*stride + (u_iref-halfPatchSize);
                 for (int j = 0; j < patchSize; j++, img_ptr++, pixel_count++) {
-                    mRefPatchCache[feature_cnt, pixel_count] = \
+                    mRefPatchCache(feature_cnt, pixel_count) = \
                         w_ref_tl*img_ptr[0] + w_ref_tr*img_ptr[1] + w_ref_bl*img_ptr[stride] + w_ref_br*img_ptr[stride+1];
                     
                     float dx = 0.5f*((w_ref_tl*img_ptr[1]  + w_ref_tr*img_ptr[2] + w_ref_bl*img_ptr[stride+1] + w_ref_br*img_ptr[stride+2])
@@ -82,18 +122,18 @@ namespace mSVO {
 
         auto& features = refFrame->obs();
         float chi2 = 0;
-        int feature_cnt = 0, n_cnt = 0;
+        int feature_cnt = 0;
         for (auto begin = features.begin(); begin != features.end(); ++begin, ++feature_cnt) {
             if (!mVisables[feature_cnt])
                 continue;
             
             Vector3f& wxyz = (*begin)->mLandmark->xyz();
             float depth = (wxyz - pos).norm();
-            Vector3f rxyz = (*begin)->mDirect*depth;
+            Vector3d rxyz = ((*begin)->mDirect*depth).cast<double>();
 
-            Vector3f cxyz = mTc_r_new*rxyz;
-            Vector2f cuv  = cxyz.head<2>()/cxyz(2);
-            Vector2f fPx  = cuv*scale;
+            Vector3d cxyz = mTc_r_new*rxyz;
+            Vector2d cuv  = cxyz.head<2>()/cxyz(2);
+            Vector2d fPx  = cuv*scale;
             const float u_fref = fPx(0),             v_fref = fPx(1);
             const int   u_iref = (int)floor(fPx(0)), v_iref = (int)floor(fPx(1));
             // if the feature has no landmark or this point can not calculate the gradient of image
@@ -116,62 +156,125 @@ namespace mSVO {
                 for (int j = 0; j < patchSize; j++, img_ptr++, pixel_count++) {
                     float instensy = \
                         w_ref_tl*img_ptr[0] + w_ref_tr*img_ptr[1] + w_ref_bl*img_ptr[stride] + w_ref_br*img_ptr[stride+1];
-                    float res = instensy - mRefPatchCache[feature_cnt, pixel_count];
+                    float res = instensy - mRefPatchCache(feature_cnt, pixel_count);
                     float weight = 1.0f;
                     if (useWeight) 
-                        weightFunction(res/scale);
+                        weight = weightFunction(res/scale);
 
                     chi2 += res*res*weight;
-                    n_cnt ++;
+                    mInliersCnt ++;
                     if (linearSystem) {
-                        Matrix<float, 1, 6>& J = mJacobianCache.row(feature_cnt*patchArea + pixel_count);
-                        mH.noalias() += J.translation()*J*weight;
-                        mb.noalias() += J.translation()*e*weight;
+                        // mJacobianCache.row()
+                        Matrix<float, 1, 6> J(mJacobianCache.row(feature_cnt*patchArea + pixel_count));
+                        mH.noalias() += J.transpose()*J*weight;
+                        mb.noalias() -= J.transpose()*res*weight;
                     }
                 }
             }
         }
-        return chi2 / n_cnt;
+        return chi2 / mInliersCnt;
     }
 
-    bool ImageAlign::solve(Sophus::SE3& new_model, Sophus::SE3& old_model) {
-        Matrix<float, 6, 1> delta_x = mH.ldlt().solve(mb);
-        if (isnan(delta_x[0]) or isinf(delta_x[0])) {
+    bool ImageAlign::solve() {
+        mDeltaX = mH.ldlt().solve(mb);
+        if (isnan(mDeltaX[0]) or isinf(mDeltaX[0])) {
             return false;
         }
-        Matrix<double, 6, 1> ddelta_x = delta_x.cast<double>();
-        new_model = old_model*Sophus::SE3::exp(-ddelta_x); 
         return true;
     }
 
+    bool ImageAlign::update(Sophus::SE3& new_model, Sophus::SE3& old_model) {
+        Matrix<double, 6, 1> ddeltaX = mDeltaX.cast<double>();
+        new_model = old_model*Sophus::SE3::exp(-ddeltaX); 
+    }
+
+    float ImageAlign::maxLimit(Matrix<float, 6, 1>& x) {
+        float max_ele = 0;
+        for (int i = 0; i < 6; i++)
+        if (fabs(x[i]) > max_ele)
+            max_ele = fabs(x[i]);
+        return max_ele;
+    }
+
+    bool ImageAlign::reset() { 
+        chi2 = -1;
+        mInliersCnt = 0; 
+        mH.setZero();
+        mb.setZero();
+        return true;
+    }
+
+    float ImageAlign::weightFunction(float res) {
+        const float k = 1.345f;
+        const float res_abs = std::abs(res);
+        if(res_abs < k)
+            return 1.0f;
+        else
+            return k / res_abs;
+    }
+
     void ImageAlign::optimize(FramePtr refFrame, FramePtr curFrame, int level) {
+        bool verbose = Config::verbose();
         // prepare data
         prepareData(refFrame, level);
         // main loop
-        float chi2_old = FLT_MAX;
-        float mu = 0, ro = 0;
+        reset();
+        chi2 = computeError(refFrame, curFrame, level, false, true);
+        float mu = -1, ro = 0, v = 2;
+        if(mu < 0){
+            double H_max_diag = 0;
+            double tau = 1e-4;
+            for(size_t j=0; j<mH.rows(); ++j)
+                H_max_diag = std::max(H_max_diag, (double)fabs(mH(j,j)));
+            mu = tau*H_max_diag;
+        }
+
+        bool stop = false;
+        float rho = -1;
         for (int i=0; i<mIterCnt; i++) {
-            mH.setZero();
-            mb.setZero();
-            // calculate new chi2
-            chi2_old = computeError(refFrame, curFrame, level, true, true);
-            // LM
-            mH += (mH.diagonal()*mu).asDiagonal();
-            // solve 
-            if (solve(mTc_r_new, mTc_r_old)) {
-                // success
-                float chi2_new = computeError(refFrame, curFrame, level, false, true);
-                rho = chi2_new - chi2_old;
-            } else {
-                rho = -1;
-            }
-            // adjust the mu
-            if (rho > 0) {
+            int failCnt = 0;
+            do {
+                reset();
+                // build linear function H b
+                computeError(refFrame, curFrame, level, true, true);
+                // LM
+                mH += (mH.diagonal()*mu).asDiagonal();
+                // solve 
+                float chi2_new = 0;
                 
-                mTc_r_old = mTc_r_new;
-            } else {
-                mTc_r_new = mTc_r_old;
-            }
+                if (solve()) {
+                    update(mTc_r_new, mTc_r_old);
+                    // success
+                    mInliersCnt = 0;
+                    // recalculate the F(x)
+                    chi2_new = computeError(refFrame, curFrame, level, false, true);
+                    rho = chi2_new - chi2;
+                } else {
+                    rho = -1;
+                }
+                // adjust the mu
+                if (rho > 0) {
+                    // update old model
+                    mTc_r_old = mTc_r_new;
+                    // update chi2, chi2 mean the value of F(x)
+                    chi2 = chi2_new;
+                    // stop condition
+                    stop = maxLimit(mDeltaX)<=meps;
+                    // update mu and v
+                    mu  *= max(1./3., min(1.-pow(2*rho-1,3), 2./3.));
+                    v   = 2.;
+                } else {
+                    // reset the Tcr
+                    mTc_r_new = mTc_r_old;
+                    // update mu
+                    mu = mu*v;
+                    v  = v*2;
+                    if (failCnt++ > mMaxFailThr)
+                        stop = true;
+                }
+            } while (!(rho>0 || stop));
+            // if stop, break
+            if (stop) break;
         }
     }
 }
--- src/image_align.hpp
+++ src/image_align.hpp
@@ -8,6 +8,7 @@
 
 #include "frame.hpp"
 #include "feature.hpp"
+#include "landmark.hpp"
 
 namespace mSVO {
     using namespace std;
@@ -16,17 +17,20 @@ namespace mSVO {
 
     class ImageAlign {
     private:
-        int mIterCnt, mMinLevel, mMaxLevel;
+        int mInliersCnt;
+        int mIterCnt, mMinLevel, mMaxLevel, mMaxFailThr;
         static const int halfPatchSize = 2;
         static const int patchSize = halfPatchSize*2;
         static const int patchArea = patchSize*patchSize;
-        
+        float meps, chi2;
+
         vector<bool> mVisables;
         MatrixXf mRefPatchCache;
         MatrixXf mJacobianCache;
 
         Matrix<float, 6, 6> mH;
         Matrix<float, 6, 1> mb;
+        Matrix<float, 6, 1> mDeltaX;
 
         Sophus::SE3 mTc_r_new;
         Sophus::SE3 mTc_r_old;
@@ -43,7 +47,10 @@ namespace mSVO {
         void prepareData(FramePtr refFrame, int level);
         void optimize(FramePtr refFrame, FramePtr curFrame, int level);
         
-        bool solve(Sophus::SE3& new_model, Sophus::SE3& old_model);
+        bool solve();
+        bool reset();
+        bool update(Sophus::SE3& new_model, Sophus::SE3& old_model);
+        float maxLimit(Matrix<float, 6, 1>& x);
         float computeError(FramePtr refFrame, FramePtr curFrame, int level, bool linearSystem, bool useWeight);
         float weightFunction(float res);
     };
